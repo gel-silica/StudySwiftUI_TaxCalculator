69f020f552efcc89a53201adf929f644f013d920
silicagel <ksk19981004@gmail.com>
2024-11-30 17:51:36 +0900

    Initial Commit

---
+++ TaxCalculator/TaxCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaxCalculatorApp.swift
+//  TaxCalculator
+//
+//  Created by silicagel on 2024/11/30.
+//
+
+import SwiftUI
+
+@main
+struct TaxCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
